7d3f30c7defd542ddfca173bfc8ce8714e2995ab
Ava Tran <ava.tran@patelhs.org>
2020-11-04 19:06:06 +0000

    I then created a String array of 15 elements.

--- Main.java
+++ Main.java
@@ -7,18 +7,17 @@ class Main {
     int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
     String[] names = new String[15];
-    
+
     Scanner scan = new Scanner(System.in);
 
     for (int i = 0; i < names.length; i++) {
-      System.out.print("What's your name");//Answer what your name is, so you can get an assigned seat
+      System.out.print("What's your name");// Answer what your name is, so you can get an assigned seat
       names[i] = scan.next();
     }
-     int count = 0;
-     while (count < 15)
-     {
-        System.out.println(names[count]);
-        count++;
-     }
+    int count = 0;
+    while (count < 15) {
+      System.out.println(names[count]);
+      count++;
+    }
   }
 }
